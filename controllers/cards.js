cc81fb32ad72278eb7ff176f58a29814c3c628a8
DmitryStarov <314263@bk.ru>
2023-07-03 01:47:01 +0800

    refactoring/обработка ошибок cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,26 +1,55 @@
 const Card = require('../models/cards');
 
+const {
+  BAD_REQUEST_STATUS,
+  NOT_FOUND_STATUS,
+  INTERNAL_SERVER_STATUS,
+  SERVER_ERROR_MESSAGE,
+  INVALID_ADD_CARD_MESSAGE,
+  CARD_NOT_FOUND_MESSAGE,
+  INVALID_LIKE_CARD_MESSAGE,
+  INVALID_ID_CARD_MESSAGE,
+} = require('../utils/constants');
+
 module.exports.getCards = (req, res) => {
   Card
     .find({})
     .then((cards) => res.send({ cards }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(INTERNAL_SERVER_STATUS).send({ message: SERVER_ERROR_MESSAGE }));
 };
 module.exports.postCard = (req, res) => {
   const { name, link } = req.body;
   Card
     .create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.send({ data: card });
+      res.send({ card });
     })
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: INVALID_ADD_CARD_MESSAGE });
+        return;
+      }
+      res.status(INTERNAL_SERVER_STATUS).send({ message: SERVER_ERROR_MESSAGE });
+    });
 };
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
   Card
     .findByIdAndRemove(cardId)
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND_STATUS).send({ message: CARD_NOT_FOUND_MESSAGE });
+        return;
+      }
+      res.send({ card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(NOT_FOUND_STATUS).send({ message: CARD_NOT_FOUND_MESSAGE });
+        return;
+      }
+      res.status(INTERNAL_SERVER_STATUS).send({ message: SERVER_ERROR_MESSAGE });
+    });
 };
 
 module.exports.putLike = (req, res) => {
@@ -31,8 +60,24 @@ module.exports.putLike = (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true },
     )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND_STATUS).send({ message: INVALID_ID_CARD_MESSAGE });
+        return;
+      }
+      res.send({ card });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: INVALID_LIKE_CARD_MESSAGE });
+        return;
+      }
+      if (err.name === 'CastError') {
+        res.status(NOT_FOUND_STATUS).send({ message: INVALID_ID_CARD_MESSAGE });
+        return;
+      }
+      res.status(INTERNAL_SERVER_STATUS).send({ message: SERVER_ERROR_MESSAGE });
+    });
 };
 module.exports.deleteLike = (req, res) => {
   const { cardId } = req.params;
@@ -42,6 +87,22 @@ module.exports.deleteLike = (req, res) => {
       { $pull: { likes: req.user._id } },
       { new: true },
     )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND_STATUS).send({ message: INVALID_ID_CARD_MESSAGE });
+        return;
+      }
+      res.send({ card });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: INVALID_LIKE_CARD_MESSAGE });
+        return;
+      }
+      if (err.name === 'CastError') {
+        res.status(NOT_FOUND_STATUS).send({ message: INVALID_ID_CARD_MESSAGE });
+        return;
+      }
+      res.status(INTERNAL_SERVER_STATUS).send({ message: SERVER_ERROR_MESSAGE });
+    });
 };
