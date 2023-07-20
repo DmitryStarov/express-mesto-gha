9087e2ad25550edce0ea737914be4d73eb1c09c7
DmitryStarov <314263@bk.ru>
2023-07-20 10:55:30 +0800

    bugfix/delete Card

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,6 +37,7 @@ module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   Card
     .findById(cardId)
+    .populate(['owner', 'likes'])
     // eslint-disable-next-line consistent-return
     .then((card) => {
       if (!card) {
