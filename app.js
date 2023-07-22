ea0a84b7e2dde1849eba40a2e00f315cd3d82be3
DmitryStarov <314263@bk.ru>
2023-07-23 02:31:19 +0800

    bugfix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-const URL = 'mongodb://localhost:27017/mestodb';
+const URL = 'mongodb://127.0.0.1:27017/mestodb';
 const { PORT = 3000 } = process.env;
 mongoose.connect(URL);
 app.use(express.json());
